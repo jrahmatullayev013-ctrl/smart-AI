b4a4a55af04126dc8d24f1cbbf6aa5aac2b23d6e
jasurbek <jrahmatullayev013@gmail.com>
2026-05-17 19:35:31 +0500

    Update brain.py

--- brain.py
+++ brain.py
@@ -1,17 +1,13 @@
 import streamlit as st
 import requests
 
-# GitHub bloklamasligi uchun kalitni bo'laklarga bo'lib yozamiz
-part1 = "hf_"
-part2 = "ACzXNvuDKWOCQRkxIPjMMzChpoEgEcfSwq"
-FINAL_TOKEN = part1 + part2
+# Kalitni kod ichidan butunlay yo'qotdik, endi uni Secrets'dan o'qiydi
+HF_TOKEN = st.secrets["HF_TOKEN"]
 
-# Hugging Face server manzili
 API_URL = "https://api-inference.huggingface.co/models/Mistralai/Mistral-7B-Instruct-v0.2"
-headers = {"Authorization": f"Bearer {FINAL_TOKEN}"}
+headers = {"Authorization": f"Bearer {HF_TOKEN}"}
 
 def get_ai_answer(user_input):
-    """Siz yozgan har qanday savolga Hugging Face orqali to'g'ridan-to'g'ri javob berish"""
     try:
         payload = {"inputs": f"<s>[INST] {user_input} [/INST]"}
         response = requests.post(API_URL, headers=headers, json=payload)
@@ -21,6 +17,6 @@ def get_ai_answer(user_input):
             full_text = output[0].get('generated_text', '')
             answer = full_text.split('[/INST]')[-1].strip()
             return answer
-        return "Kechirasiz, muloqotda uzilish bo'ldi. Qayta yozib ko'ring."
+        return "Kechirasiz, muloqotda uzilish bo'ldi."
     except Exception as e:
         return f"Mening miyamda texnik nosozlik: {str(e)}"
